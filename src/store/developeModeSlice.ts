6cf5172f660bdc50ec1ec5a7dc6e035b639669f9
wyMinLwin <waiyanminlwin421@gmail.com>
2023-12-11 21:07:36 +0630

    Minor change

--- src/store/developeModeSlice.ts
+++ src/store/developeModeSlice.ts
@@ -1,14 +1,14 @@
-import {createSlice} from '@reduxjs/toolkit';
-const initialState:boolean = false;
+import { createSlice } from "@reduxjs/toolkit";
+const initialState: boolean = true;
 const developerModeSlice = createSlice({
-    name:"DeveloperMode",
-    initialState,
-    reducers: {
-        toggleDeveloperMode(state) {
-            return !state; 
-        }
-    }
-})
+	name: "DeveloperMode",
+	initialState,
+	reducers: {
+		toggleDeveloperMode(state) {
+			return !state;
+		},
+	},
+});
 
-export const {toggleDeveloperMode} = developerModeSlice.actions;
+export const { toggleDeveloperMode } = developerModeSlice.actions;
 export default developerModeSlice.reducer;
